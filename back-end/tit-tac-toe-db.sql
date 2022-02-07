6fb5833cc1145b435ac7044ced9b3a6c6a65a800
misa vojtova <misacastella@gmail.com>
2022-02-07 23:12:01 +0000

    tic-tac-toe-db.sql

--- back-end/tit-tac-toe-db.sql
+++ back-end/tit-tac-toe-db.sql
@@ -1,22 +1,22 @@
 create database tictac;
-use tictac;
-
+n
 DROP TABLE IF EXISTS tictac;
+use tictac;
 CREATE TABLE tictac(
 `id`INT primary key auto_increment ,
 `box_id` varchar(50) ,
-`output` varchar(50));
+`input` varchar(50));
+select * from tictac;
 
 
-
-insert into tictac (`box_id`, `output`) values ( 'zero', '');
-insert into tictac ( `box_id`, `output`) values ( 'one', '');
-insert into tictac ( `box_id`, `output`) values ('two', '');
-insert into tictac ( `box_id`, `output`) values ('three', '');
-insert into tictac ( `box_id`, `output`) values ('four', '');
-insert into tictac ( `box_id`, `output`) values ('five', '');
-insert into tictac ( `box_id`, `output`) values ('six', '');
-insert into tictac ( `box_id`, `output`) values ('seven', '');
-insert into tictac (`box_id`, `output`) values ('eight', '');
+insert into tictac (`box_id`, `input`) values ( 'zero', '');
+insert into tictac ( `box_id`, `input`) values ( 'one', '');
+insert into tictac ( `box_id`, `input`) values ('two', '');
+insert into tictac ( `box_id`, `input`) values ('three', '');
+insert into tictac ( `box_id`, `input`) values ('four', '');
+insert into tictac ( `box_id`, `input`) values ('five', '');
+insert into tictac ( `box_id`, `input`) values ('six', '');
+insert into tictac ( `box_id`, `input`) values ('seven', '');
+insert into tictac (`box_id`, `input`) values ('eight', '');
 
 
